4e00cfa39af6cd602e77ed533064e3909b18825b
DESKTOP-AMC8OOC\Sunnyngai <arunwongdotw@gmail.com>
2018-11-14 14:21:25 +0700

    Add fee value.

--- www/js/controllers/addPointController.js
+++ www/js/controllers/addPointController.js
@@ -22,56 +22,85 @@ appControllers.controller('addPointCtrl', function($scope, $timeout, $state, $st
     if (($scope.point.name != null) && ($scope.point.name != "")) {
       if (($scope.point.price != null) && ($scope.point.price != "")) {
         if (checkNumberRegEx.test($scope.point.price)) {
-          $mdDialog.show({
-            controller: 'DialogController',
-            templateUrl: 'confirm-dialog.html',
-            locals: {
-              displayOption: {
-                title: "เพิ่มจุดลงรถตู้ ?",
-                content: "คุณแน่ใจที่จะเพิ่มจุดลงรถตู้",
-                ok: "ตกลง",
-                cancel: "ยกเลิก"
-              }
+          if (($scope.point.fee != null) && ($scope.point.fee != "")) {
+            if (checkNumberRegEx.test($scope.point.fee)) {
+              $mdDialog.show({
+                controller: 'DialogController',
+                templateUrl: 'confirm-dialog.html',
+                locals: {
+                  displayOption: {
+                    title: "เพิ่มจุดลงรถตู้ ?",
+                    content: "คุณแน่ใจที่จะเพิ่มจุดลงรถตู้",
+                    ok: "ตกลง",
+                    cancel: "ยกเลิก"
+                  }
+                }
+              }).then(function(response) {
+                $http({
+                  url: myService.configAPI.webserviceURL + 'webservices/addPoint.php',
+                  method: 'POST',
+                  data: {
+                    var_memberid: myService.memberDetailFromLogin.member_id,
+                    var_name: $scope.point.name,
+                    var_price: $scope.point.price,
+                    var_fee: $scope.point.fee
+                  }
+                }).then(function(response) {
+                  $mdDialog.show({
+                    controller: 'DialogController',
+                    templateUrl: 'confirm-dialog.html',
+                    locals: {
+                      displayOption: {
+                        title: "เพิ่มจุดลงรถตู้สำเร็จ !",
+                        content: "คุณเพิ่มจุดลงรถตู้สำเร็จ",
+                        ok: "ตกลง"
+                      }
+                    }
+                  }).then(function(response) {
+                    $state.go('loginown.ownpointlist');
+                  });
+                }, function(error) {
+                  $mdDialog.show({
+                    controller: 'DialogController',
+                    templateUrl: 'confirm-dialog.html',
+                    locals: {
+                      displayOption: {
+                        title: "เกิดข้อผิดพลาด !",
+                        content: "เกิดข้อผิดพลาด btnAddPoint ใน addPointController ระบบจะปิดอัตโนมัติ",
+                        ok: "ตกลง"
+                      }
+                    }
+                  }).then(function(response) {
+                    ionic.Platform.exitApp();
+                  });
+                });
+              });
+            } else {
+              $mdDialog.show({
+                controller: 'DialogController',
+                templateUrl: 'confirm-dialog.html',
+                locals: {
+                  displayOption: {
+                    title: "ราคาค่านำเนียมไม่ถูกต้อง !",
+                    content: "ราคาค่าทำเนียม ต้องเป็นตัวเลขเท่านั้น",
+                    ok: "ตกลง"
+                  }
+                }
+              });
             }
-          }).then(function(response) {
-            $http({
-              url: myService.configAPI.webserviceURL + 'webservices/addPoint.php',
-              method: 'POST',
-              data: {
-                var_memberid: myService.memberDetailFromLogin.member_id,
-                var_name: $scope.point.name,
-                var_price: $scope.point.price
+          } else {
+            $mdDialog.show({
+              controller: 'DialogController',
+              templateUrl: 'confirm-dialog.html',
+              locals: {
+                displayOption: {
+                  title: "ราคาค่านำเนียมไม่ถูกต้อง !",
+                  content: "กรุณากรอกราคาค่าทำเนียม",
+                  ok: "ตกลง"
+                }
               }
-            }).then(function(response) {
-              $mdDialog.show({
-                controller: 'DialogController',
-                templateUrl: 'confirm-dialog.html',
-                locals: {
-                  displayOption: {
-                    title: "เพิ่มจุดลงรถตู้สำเร็จ !",
-                    content: "คุณเพิ่มจุดลงรถตู้สำเร็จ",
-                    ok: "ตกลง"
-                  }
-                }
-              }).then(function(response) {
-                $state.go('loginown.ownpointlist');
-              });
-            }, function(error) {
-              $mdDialog.show({
-                controller: 'DialogController',
-                templateUrl: 'confirm-dialog.html',
-                locals: {
-                  displayOption: {
-                    title: "เกิดข้อผิดพลาด !",
-                    content: "เกิดข้อผิดพลาด btnAddPoint ใน addPointController ระบบจะปิดอัตโนมัติ",
-                    ok: "ตกลง"
-                  }
-                }
-              }).then(function(response) {
-                ionic.Platform.exitApp();
-              });
             });
-          });
+          }
         } else {
           $mdDialog.show({
             controller: 'DialogController',
